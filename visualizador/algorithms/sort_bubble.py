62650f05214ca0db4d62e481fb80ed95560f1ae8
JuanMansilla1 <juan.mansilla2009@gmail.com>
2025-11-20 20:26:43 -0300

    inicio bubble

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -1,23 +1,30 @@
 # Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
 
-items = []
-n = 0
-i = 0
-j = 0
+items = [5,10,40,2,9,6,1,3,50,32,25,99,74,82,7,8,16,27,90,45]
+i = 0 # CONTADOR
+j = 0 # INDICE
+n = len(items) 
+swapCont = 0
 
-def init(vals):
-    global items, n, i, j
-    items = list(vals)
-    n = len(items)
-    i = 0
-    j = 0
+def init(lista):
+    copiaItems = items
+    n = len(items) 
+    i = 0 # Contador de pasadas
+    j = 0 # Pos en cada pasada
+    done = False # Si termino de ordenar
+    swap = False # Intercambio
+    swapCont = 0
 
 def step():
-    # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+    swap = False
+    if items[j] > items[j+1]:
+        items[j],items[j+1] = items[j+1],items[j]
+        swap = True     
+        swapCont += 1        
+        return {"j": j, "j+1": j+1, "swap": swap, "done":False}
+    j += 1
+    if j+1 == n - i - 1:
+        j = 0
+        i += 1
+    if i >= n - 1:
+        return {"done": True}
